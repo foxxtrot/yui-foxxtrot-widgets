d9a1a9c58dbf928d3be1e6f79d0e23b569e12879
Jeff Craig <foxxtrot@foxxtrot.net>
2008-12-04 17:57:55 -0800

    Added YUI Test Tests for automated unit-testing.

--- src/Foxxtrot/Widgets/Laconica.js
+++ src/Foxxtrot/Widgets/Laconica.js
@@ -14,63 +14,8 @@
 if (typeof Foxxtrot === 'undefined' || !Foxxtrot) { Foxxtrot = {}; }
 Foxxtrot.Widgets = Foxxtrot.Widgets || {};
 Foxxtrot.Widgets.Laconica = function () {
-    var _target, _svc;
+    var _target, _svc = "";
     // Requires YUI 3.0
-    /**
-     * _timePhrase is a private function which calculates what to print for the time
-     * of the post. Taken from Laconica 0.6.1 lib/util.php
-     * @method _timePhrase
-     * @argument date
-     **/
-    var _timePhrase = function(date) {
-        var delta = (new Date().getTime() - new Date(date).getTime())/1000;
-        if (delta < 60) {
-            return 'a few seconds ago';
-        } else if (delta < 92) {
-            return 'about a minute ago';
-        } else if (delta < 3300) {
-            return 'about ' + Math.round(delta/60) + ' minutes ago';
-        } else if (delta < 5400) {
-            return 'about an hour ago';
-        } else if (delta < 79200) {
-            return 'about ' + Math.round(delta/3600) + ' hours ago';
-        } else if (delta < 133200) {
-            return 'about a day ago';
-        } else if (delta < 2073600) {
-            return 'about ' + Math.round(delta/86400) + ' days ago';
-        } else if (delta < 3974400) {
-            return 'about a month ago';
-        } else if (delta < 28512000) {
-            return 'about ' + Math.round(delta/259200) + ' months ago';
-        } else if (delta < 41472000) {
-            return 'about a year ago';
-        } else {
-            return 'a very long time ago';
-        }
-    };
-    
-    var _userLink = function(msg) {
-    	var le = /@(\w+)/g, i;
-    	var r = msg.match(le), s;
-        
-        for (i = 0 ; r && i < r.length ; i += 1) {
-            s = r[i].substr(1);
-            s = '@<a href="' + _svc + s + '">' + s + '</a>';
-            msg = msg.replace(r[i],s);
-        }
-        return msg;
-    };
-    
-    var _uriLink = function(msg) {
-        var le = /([A-Za-z]+\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g;
-        var r = msg.match(le), s, i;
-        for (i = 0 ; r && i < r.length ; i += 1) {
-            s = '<a href="' + r[i] + '">' + r[i] + '</a>';
-            msg = msg.replace(r[i],s);
-        }
-        return msg;
-    };
-
     return {
 				/**
 				 * updatesCallback is a public function which handles the JSONP callback
@@ -122,6 +67,58 @@ Foxxtrot.Widgets.Laconica = function () {
 
             URL += "callback=" + (o.callback || "Foxxtrot.Widgets.Laconica.updatesCallback");
             YUI().Get.script(URL);
-        }
+        },
+        /**
+		     * _timePhrase is a private function which calculates what to print for the time
+		     * of the post. Taken from Laconica 0.6.1 lib/util.php
+		     * @method _timePhrase
+		     * @argument date
+		     **/
+        _timePhrase: function(date) {
+	        var delta = (new Date().getTime() - new Date(date).getTime())/1000;
+	        if (delta < 60) {
+	            return 'a few seconds ago';
+	        } else if (delta < 92) {
+	            return 'about a minute ago';
+	        } else if (delta < 3300) {
+	            return 'about ' + Math.round(delta/60) + ' minutes ago';
+	        } else if (delta < 5400) {
+	            return 'about an hour ago';
+	        } else if (delta < 79200) {
+	            return 'about ' + Math.round(delta/3600) + ' hours ago';
+	        } else if (delta < 133200) {
+	            return 'about a day ago';
+	        } else if (delta < 2073600) {
+	            return 'about ' + Math.round(delta/86400) + ' days ago';
+	        } else if (delta < 3974400) {
+	            return 'about a month ago';
+	        } else if (delta < 28512000) {
+	            return 'about ' + Math.round(delta/259200) + ' months ago';
+	        } else if (delta < 41472000) {
+	            return 'about a year ago';
+	        } else {
+	            return 'a very long time ago';
+	        }
+				},
+			_userLink: function(msg) {
+				var le = /@(\w+)/g, i;
+				var r = msg.match(le), s;
+
+				for (i = 0 ; r && i < r.length ; i += 1) {
+					s = r[i].substr(1);
+					s = '@<a href="' + (_svc || "/") + s + '">' + s + '</a>';
+					msg = msg.replace(r[i],s);
+				}
+				return msg;
+	    },
+			_uriLink: function(msg) {
+				var le = /([A-Za-z]+\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g;
+				var r = msg.match(le), s, i;
+				for (i = 0 ; r && i < r.length ; i += 1) {
+					s = '<a href="' + r[i] + '">' + r[i] + '</a>';
+					msg = msg.replace(r[i],s);
+				}
+				return msg;
+			}
     };
 }();
